c257ef79d37492ae988e067b4a37c9acf55a4ca2
Moody <269313188@qq.com>
2022-10-28 23:38:37 +0800

    BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -43,15 +43,6 @@ export default ({ mode }) => {
       },
       //警报门槛，限制大文件大小
       chunkSizeWarningLimit: 1500,
-      rollupOptions: {
-        output: {
-          //对静态文件进行打包处理（文件分类）
-          //此处打开后会导致背景图路径有问题，所以暂时隐藏，未找到合适的解决方案
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          entryFileNames: 'assets/js/[name]-[hash].js',
-          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-        }
-      },
     },
     server: {
       open: true,
